a0321e070e68d4e85c1f1bfbe2972be67b9a1bd6
tsteele1 <tylersteele557@gmail.com>
2024-03-06 19:03:07 -0700

    Finished deleteCityTest. Should pass.

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -69,5 +69,8 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public void deleteCity(City city) {
+        if (cities.contains(city)) {
+            cities.remove(city);
+        }
     }
 }
